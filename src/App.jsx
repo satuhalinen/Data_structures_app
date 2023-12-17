6d8ef35374fa2c43abe9e2739246507a3e11f465
[satuhalinen] <satuh9@gmail.com>
2023-12-17 22:42:40 +0200

    make React rooting for home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import "./App.css";
+import Root from "./pages/Root";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+  ]);
   return (
     <>
+      <RouterProvider router={router} />
       <h1>Data structures app</h1>
-      <p className="welcomeText">Choose the app you want discover more:</p>
     </>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <p className="welcomeText">Choose the app you want discover more:</p>;
+};
+
+export default Home;
